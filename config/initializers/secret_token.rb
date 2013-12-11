658e87352fd3b740fdb144647678d071250a6a57
dannyyu92 <dannyyu92@server.fake>
2013-12-10 23:25:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JackySite::Application.config.secret_key_base = '723426f44f272dbb729815a9502aeac755eb7471a859b3e415e56b4f41494eeed0cf1629e0da37bd3b94458d765b8814735e38288a3672610c1150fdd32cdd84'
