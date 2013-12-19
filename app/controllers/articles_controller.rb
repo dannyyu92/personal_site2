79a37922adc014d1827178a41c911f84ecf62168
dannyyu92 <dannyyu92@server.fake>
2013-12-19 15:23:54 -0500

    Add working form for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,18 +4,44 @@ class ArticlesController < ApplicationController
 	end
 
   def new
-  	@article = Article.new;
+  	@article = Article.new
   end
 
   def show
-  	@article = Article.find(params[:id]);
+  	@article = Article.find(params[:id])
   end
 
   def edit
-  	 @article = Article.find(params[:id]);
+  	 @article = Article.find(params[:id])
   end
 
   def destroy
-  	 @article = Article.find(params[:id]);
+  	@article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_url
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: "Article successfully created"
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(article_params)
+      redirect_to @article, notice: "Article successfully update"
+    else
+      render action: "edit"
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:name, :posttype, :body, :date)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ActiveRecord::Base
+	validates :name, :body, :date, presence: true
 end
